2af9d3c6eea369dd8bf9090d225ad87f50e51c22
Ramiro Alves <ramiro.junior25@gmail.com>
2023-11-26 11:30:50 -0300

    First commit

---
+++ src/Controllers/books/books.controller.ts
@@ -0,0 +1,27 @@
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+
+import { BookDTO } from '../../DTO/books.dto'
+
+@Controller('books')
+export class BooksController {
+    @Get()
+    getAllBooks(): string {
+        return 'Todos os livros aqui!';
+    }
+
+    @Post()
+    saveBook(@Body() newBook: BookDTO): BookDTO {
+        return newBook;
+    }
+
+    @Patch()
+    updateBook(): string {
+        return 'Este livro foi atualizado';
+    }
+
+    @Delete()
+    deleteBook(): string {
+        return 'Este livro foi deletado';
+    }
+}
